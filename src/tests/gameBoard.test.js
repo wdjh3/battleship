8cef63817eb40f6de0c9f46c2d611a1786e4d951
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-04-14 12:57:53 +0800

    test: validate coords in placeShip()

--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -15,6 +15,44 @@ test("gameBoard's receiveAttack() to not accept invalid coordinates", () => {
   }
 });
 
+test("gameBoard to NOT place Ship out of bounds", () => {
+  const testCases = [
+    [3, 11],
+    [4, -2],
+    [15, 4],
+    [-3, 6],
+  ];
+
+  for (const testCase of testCases) {
+    expect(gameBoard.placeShip(testCase, 4, "horizontal")).toBe(false);
+  }
+});
+
+test("gameBoard to NOT place Ship length out of bounds", () => {
+  const testCases = [
+    {
+      "coords": [7, 3],
+      "length": 4,
+      "rotation": "horizontal",
+    },
+    {
+      "coords": [7, 9],
+      "length": 4,
+      "rotation": "vertical",
+    },
+  ];
+
+  for (const testCase of testCases) {
+    expect(
+      gameBoard.placeShip(
+        testCase["coords"],
+        testCase["length"],
+        testCase["rotation"],
+      ),
+    ).toBe(false);
+  }
+});
+
 test("gameBoard to place Ship at expected coordinates", () => {
   gameBoard.placeShip([3, 3], 4);
   expect(gameBoard.board["3,3"]).toEqual({
@@ -51,6 +89,8 @@ test("gameBoard to place Ship at expected coordinates and length", () => {
     hits: 0,
     hasSunk: false,
   });
+  // Not overextend
+  expect(gameBoard.board["7,3"]).toBeUndefined();
   // Tests if they're to the same reference
   expect(gameBoard.board["3,3"] === gameBoard.board["4,3"]).toBe(true);
 });
